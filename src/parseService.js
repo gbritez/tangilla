53e25cccd0884d19934cf074ebb5eb0cf55f4bdf
Gonzalo <gonzalo_britez@hotmail.com>
2023-04-13 00:13:24 -0300

    modified parameters

--- src/parseService.js
+++ src/parseService.js
@@ -1,9 +1,9 @@
 
 import Axios from "axios"
 
-export const getSynonyms = async (formData) => {
+export const getSynonyms = async (text) => {
     try {
-        const response = await Axios.post('http://localhost:8080/post', { formData });
+        const response = await Axios.post('http://localhost:8080/post', { text });
         return response.data;
     }
     catch (error) {
